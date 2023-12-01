d0fffd409d8eb71561069b95455fd21b572a21c6
thuytrann <71290536+thuytrann@users.noreply.github.com>
2023-12-01 01:43:50 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
     var container = document.querySelector("#cont");
+    var glow = document.querySelector("#glow");
+    var maskSlider = document.getElementById('molecule');
     var activeItem = null;
 
     var active = false;
@@ -22,10 +24,10 @@
 
         if (activeItem !== null) {
           if (!activeItem.xOffset) {
-            activeItem.xOffset = 0;
+            activeItem.xOffset = 0;          
           }
 
-          if (!activeItem.yOffset) {
+          if (!activeItem.yOffset) {                                                                              
             activeItem.yOffset = 0;
           }
 
@@ -55,9 +57,11 @@
       if (active) {
         e.preventDefault();
 				if(e.type === "touchmove"){
-
+                    
           activeItem.currentX = e.touches[0].clientX - activeItem.initialX;
           activeItem.currentY = e.touches[0].clientY - activeItem.initialY;
+          
+          
         } else {
           activeItem.currentX = e.clientX - activeItem.initialX;
           activeItem.currentY = e.clientY - activeItem.initialY;
@@ -71,5 +75,93 @@
     }
 
     function setTranslate(xPos, yPos, el) {
-      el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+      el.style.transform = "translate3d(" + 0 + "px, " + 0 + "px, 0)";
+      $(document).ready(function(){
+      $('#glow').css('-webkit-mask-position-x', xPos );
+      $('#glow').css('-webkit-mask-position-y', yPos );
+    });
+    console.log(xPos)
+      /*glow.addEventListener("change", (event) => { 
+        element.style.webkitMaskPosition = xPos;
+        console.log(element.style.webkitMaskPosition,xPos);
+      });
+      console.log(glow)
+      /*var xx = xPos;
+                    var z = "-webkit-gradient(linear, left center, right center, color-stop(0.5, black), color-stop(0.5, transparent)) no-repeat scroll " + xx + "px padding padding";
+                    //el.style.webkitMask = z;
+                    el.style.webkitMaskPosition = xPos;
+                    console.log(xPos, xx, z);
+                    console.log(window.getComputedStyle(el).webkitMaskPosition);*/
+                    /*var style = document.createElement('style');
+                    var cssStr = '<style id="glow">#glow{' +
+                    '-webkit-mask-size: 200% 200%;' +
+                    '-webkit-mask-repeat: no-repeat;' +
+                    '-webkit-animation: wipe 2s;' +
+                    '-webkit-animation-direction: normal;' +
+                    '-webkit-animation-iteration-count: 1;' +
+                    '-webkit-animation-fill-mode: forwards;' +
+                    '-webkit-mask-position: offsetLeftS;' +
+                    '-webkit-mask-image: -webkit-gradient(linear, left top, right top, ' +
+                    'color-stop(0%, transparent), color-stop(20%, transparent), ' +
+                    'color-stop(25%, transparent), color-stop(30%, transparent), ' +
+                    'color-stop(50%, rgba(0, 0, 0, 1)), color-stop(98%, rgba(0, 0, 0, 1)), ' +
+                    'color-stop(100%, rgba(0, 0, 0, 1)));}' +
+                    '@-webkit-keyframes wipe {' +
+                    '0% { -webkit-mask-position: offsetLeftS 0; }' +
+                    '100% { -webkit-mask-position: offsetLeftD 0; }' +
+                    '}' +
+                    '</style>';
+                    cssStr = cssStr.replace(/offsetLeftS/g, xPos + 'px');
+                cssStr = cssStr.replace(/offsetLeftD/g, xPos + 'px');
+                    style.setAttribute("type", "text/css");
+                    var head = document.getElementsByTagName('head')[0];
+                    head.appendChild(style);
+                    if (style.styleSheet)
+                    {   // IE only
+                        style.styleSheet.cssText = cssStr;
+                    }
+                    else
+                    {   // other browsers
+                        console.log(style, cssStr );
+                        console.log(window.getComputedStyle(el).webkitMaskPosition);
+                        style.appendChild(document.createTextNode(cssStr));
+                    }
+                /*var compStyle = getComputedStyle(el);
+                var width = parseInt(compStyle.getPropertyValue("width"));
+                var height = parseInt(compStyle.getPropertyValue("height"));
+                cssStr = cssStr.replace(/offsetLeftS/g, xPos + 'px');
+                cssStr = cssStr.replace(/offsetLeftD/g, xPos + 'px');
+                console.log(cssStr);
+                document.head.append(cssStr);
+                
+               /*
+                var linkElement = this.document.createElement('link');
+                var cssStr = '<style id="glow">#glow{' +
+                '-webkit-mask-size: 200% 200%;' +
+                '-webkit-mask-repeat: no-repeat;' +
+                '-webkit-animation: wipe 2s;' +
+                '-webkit-animation-direction: normal;' +
+                '-webkit-animation-iteration-count: 1;' +
+                '-webkit-animation-fill-mode: forwards;' +
+                '-webkit-mask-position: offsetLeftS;' +
+                '-webkit-mask-image: -webkit-gradient(linear, left top, right top, ' +
+                'color-stop(0%, transparent), color-stop(20%, transparent), ' +
+                'color-stop(25%, transparent), color-stop(30%, transparent), ' +
+                'color-stop(50%, rgba(0, 0, 0, 1)), color-stop(98%, rgba(0, 0, 0, 1)), ' +
+                'color-stop(100%, rgba(0, 0, 0, 1)));}' +
+                '@-webkit-keyframes wipe {' +
+                '0% { -webkit-mask-position: offsetLeftS 0; }' +
+                '100% { -webkit-mask-position: offsetLeftD 0; }' +
+                '}' +
+                '</style>';
+            var compStyle = getComputedStyle(el);
+            var width = parseInt(compStyle.getPropertyValue("width"));
+            var height = parseInt(compStyle.getPropertyValue("height"));
+            cssStr = cssStr.replace(/offsetLeftS/g, xPos + 'px');
+            cssStr = cssStr.replace(/offsetLeftD/g, xPos + 'px');
+
+                linkElement.setAttribute('rel', 'stylesheet');
+                linkElement.setAttribute('type', 'text/css');
+                linkElement.setAttribute('href', 'data:text/css;charset=UTF-8,' + encodeURIComponent(cssStr));
+                */
     }
